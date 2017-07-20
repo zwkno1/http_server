a94318c3952f47ee803a8ba07af4d3dd0cbd2044
zhouweikang <zhouweikang@dianjiutong.cn>
2017-07-20 11:45:36 +0800

    fix parse update error

--- src/tgbot/TgParser.h
+++ src/tgbot/TgParser.h
@@ -7,37 +7,39 @@ class TgParser
 public:
     std::vector<TgBot::Update::Ptr> parseUpdate(const std::string & updateString, bool longPolling = false)
     {
-        boost::property_tree::ptree pt = parseString(updateString);
-        if(longPolling)
+        try
         {
-            if (pt.get<bool>("ok", false))
+            boost::property_tree::ptree pt = parseString(updateString);
+            if(longPolling)
             {
-                pt = pt.get_child("result");
+                if (pt.get<bool>("ok", false))
+                {
+                    pt = pt.get_child("result");
+                    return parser_.parseJsonAndGetArray<TgBot::Update>(&TgBot::TgTypeParser::parseJsonAndGetUpdate, pt);
+                }
+                else
+                {
+                    Log::debug() << "parseUpdate error, long pooling not ok";
+                }
             }
             else
             {
-                Log::debug() << "parseUpdate error, long pooling not ok";
-                return std::vector<TgBot::Update::Ptr>{};
+                return std::vector<TgBot::Update::Ptr>{ parser_.parseJsonAndGetUpdate(pt) };
             }
         }
+        catch (boost::property_tree::ptree_error& e)
+        {
+            Log::debug() << "parseUpdate error: " << e.what();
+        }
 
-        return parser_.parseJsonAndGetArray<TgBot::Update>(&TgBot::TgTypeParser::parseJsonAndGetUpdate, pt);
+        return std::vector<TgBot::Update::Ptr>{ };
     }
 
 private:
     boost::property_tree::ptree parseString(const std::string & str)
     {
-        try
-        {
-            boost::property_tree::ptree result = parser_.parseJson(str);
-            return result;
-        }
-        catch (boost::property_tree::ptree_error& e)
-        {
-           Log::debug() << "parseString error: " << e.what();
-        }
-
-        return boost::property_tree::ptree{};
+        boost::property_tree::ptree result = parser_.parseJson(str);
+        return result;
     }
 
     TgBot::TgTypeParser parser_;
