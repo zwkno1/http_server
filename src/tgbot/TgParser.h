4d000938e50e04f2fd09f21e738a5670de8f7c53
zhouweikang <zhouweikang@dianjiutong.cn>
2017-07-20 11:22:41 +0800

    fix parse update error

--- src/tgbot/TgParser.h
+++ src/tgbot/TgParser.h
@@ -5,26 +5,32 @@
 class TgParser
 {
 public:
-    std::vector<TgBot::Update::Ptr> parseUpdate(const std::string & updateString)
+    std::vector<TgBot::Update::Ptr> parseUpdate(const std::string & updateString, bool longPolling = false)
     {
         boost::property_tree::ptree pt = parseString(updateString);
+        if(longPolling)
+        {
+            if (pt.get<bool>("ok", false))
+            {
+                pt = pt.get_child("result");
+            }
+            else
+            {
+                Log::debug() << "parseUpdate error, long pooling not ok";
+                return std::vector<TgBot::Update::Ptr>{};
+            }
+        }
+
         return parser_.parseJsonAndGetArray<TgBot::Update>(&TgBot::TgTypeParser::parseJsonAndGetUpdate, pt);
     }
 
 private:
     boost::property_tree::ptree parseString(const std::string & str)
     {
-        boost::property_tree::ptree result = parser_.parseJson(str);
         try
         {
-            if (result.get<bool>("ok", false))
-            {
-                return result.get_child("result");
-            }
-            else
-            {
-                Log::debug() << "parseString error: " << result.get("description", "");
-            }
+            boost::property_tree::ptree result = parser_.parseJson(str);
+            return result;
         }
         catch (boost::property_tree::ptree_error& e)
         {
